60e45dda2ef6946db90d02200ee28a1ef14c086b
homework <ian061504@gmail.com>
2016-12-21 21:37:01 +0800

    hw

---
+++ src/Bulbasur.java
@@ -0,0 +1,17 @@
+
+public class Bulbasur extends Pokemon {
+	
+	
+	public Bulbasur(String name,PokemonType type,int cp)
+	{
+		
+		super(name,type,cp);
+	}
+	
+	
+	public void attack()
+	{
+		System.out.println("Tackle Tail");
+	}
+
+}
---
+++ src/Psyduck.java
@@ -0,0 +1,17 @@
+
+public class Psyduck extends Pokemon {
+	
+	
+	
+	public Psyduck(String name,PokemonType type,int cp)
+	{
+		
+		super(name,type,cp);
+	}
+	
+	
+	public void attack()
+	{
+		System.out.println("Aqua Tail");
+	}
+}
